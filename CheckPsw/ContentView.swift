dafca7d033f113a3c8b0adec222ab0c6fd1b6c05
workingdog <ringowathelet@gmail.com>
2023-08-09 12:55:45 +0900

    iniital commit

--- CheckPsw/ContentView.swift
+++ CheckPsw/ContentView.swift
@@ -52,8 +52,8 @@ struct ContentView: View {
     
     func askPwned(_ psw: String) async -> Int {
         let hash = getSha1(for: psw)
-        let prefix = String(hash.prefix(5))
-        let sufix = String(hash.dropFirst(5))
+        let prefix = hash.prefix(5)
+        let sufix = hash.dropFirst(5)
         
         if let url = URL(string: "https://api.pwnedpasswords.com/range/\(prefix)") {
             do {
@@ -77,90 +77,3 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
-
-
-/*
- 
- struct ContentView: View {
-     @State var psw = ""
-     @State var pswCount = 0
-     @State var msg = ""
-     
-     var body: some View {
-         VStack (spacing: 30) {
-             TextField("enter password", text: $psw).border(.red)
-                 .onSubmit {
-                     pswCount = 0
-                     msg = ""
-                     if !psw.isEmpty {
-                         Task {
-                             await checkPsw()
-                         }
-                     }
-                 }
-             Text(msg).bold().foregroundStyle(pswCount > 0 ? .red : .green)
-         }
-         .padding()
-     }
-     
-     func checkPsw() async {
-         let hash = getSha1(for: psw)
-         let prefix = String(hash.prefix(5))
-         let sufix = String(hash.dropFirst(5))
-         
-         let results = await askPwned(prefix)
-         
-         if let value = results[sufix] {
-             pswCount = value
-         } else {
-             pswCount = 0
-         }
-         
-         if pswCount > 0 {
-             msg = """
- Oh no — pwned!
-             
- This password has been seen \(pswCount) times before
- This password has previously appeared in a data breach and should never be used. If you've ever used it anywhere before, change it!
- """
-         } else {
-             msg = """
- Good news — no pwnage found!
-
- This password wasn't found in any of the Pwned Passwords loaded into Have I Been Pwned. That doesn't necessarily mean it's a good password, merely that it's not indexed on this site. If you're not already using a password manager, go and download 1Password and change all your passwords to be strong and unique.
- """
-         }
-     }
-     
-     func getSha1(for str: String) -> String {
-         Insecure.SHA1.hash(data: str.data(using: .utf8)!)
-             .map{ String(format: "%02x", $0) }.joined()
-     }
-
-     func askPwned(_ hashPrefix: String) async -> [String:Int] {
-         if let url = URL(string: "https://api.pwnedpasswords.com/range/\(hashPrefix)") {
-             do {
-                 let (data, _) = try await URLSession.shared.data(from: url)
-                 let theResponse = String(data: data, encoding: .utf8)!
-                 let lines: [String] = theResponse.components(separatedBy: "\n")
-
-                 var results: [String:Int] = [:]
-                 for line in lines {
-                     let compo = line.components(separatedBy: ":")
-                     if let first = compo.first?.trimmingCharacters(in: .whitespacesAndNewlines),
-                        let last = compo.last?.trimmingCharacters(in: .whitespacesAndNewlines),
-                        let val = Int(last) {
-                         results[first.lowercased()] = val
-                     }
-                 }
-                 return results
-             } catch {
-                 print(error)
-             }
-         }
-         return [:]
-     }
-     
- }
-
- */
